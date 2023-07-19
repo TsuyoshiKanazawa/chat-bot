06aa1d2df083a9d7e801300705efe411495c9138
Tsuyoshi Kanazawa <t.kanazawa0810@gmail.com>
2023-07-19 20:56:03 +0900

    test

--- server.js
+++ server.js
@@ -2,8 +2,6 @@ const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
 
-require('dotenv').config();
-
 const app = express();
 app.use(cors());
 app.use(express.json());
