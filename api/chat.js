2e4716a47f38b7561cc7cc895b087c4a70c9e21b
Tsuyoshi Kanazawa <t.kanazawa0810@gmail.com>
2023-08-10 11:04:20 +0900

    test

--- api/chat.js
+++ api/chat.js
@@ -8,11 +8,12 @@ exports.handler = async function (event, context) {
 
     const userInput = JSON.parse(event.body).message;
 
-    const openai = axios.create({
-        baseURL: 'https://scalar-test.openai.azure.com',
+    const azureOpenai = axios.create({
+        baseURL: 'https://scalar-test.openai.azure.com/',
         headers: {
             'Authorization': `Bearer ${process.env.OPENAI_API_KEY}`,
             'Content-Type': 'application/json',
+            'OpenAI-API-Version': '2023-03-15-preview'
         },
     });
 
@@ -24,18 +25,22 @@ exports.handler = async function (event, context) {
     // チャット履歴を更新する
     chatHistory.push(userMessage);
 
-    console.log("User message:", userMessage); // 追加
+    console.log("User message:", userMessage);
 
     const messages = [...chatHistory];
 
     try {
-        const response = await openai.post('/openai/deployments/Scalar/chat/completions?api-version=2023-03-15-preview', {
-            model: "gpt-3.5-turbo-16k",
-            messages: messages
+        const response = await azureOpenai.post('/v1/engines/Scalar/completions', {
+            messages: messages,
+            temperature: 0.7,
+            max_tokens: 800,
+            top_p: 0.95,
+            frequency_penalty: 0,
+            presence_penalty: 0
         });
         const assistantMessage = response.data.choices[0].message.content;
 
-        console.log("Assistant message:", assistantMessage); // 追加
+        console.log("Assistant message:", assistantMessage);
 
         // ボットの応答をチャット履歴に追加
         const botMessage = {
