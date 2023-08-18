c3f47698b51c71ea974b84c4018e145402849ed9
Tsuyoshi Kanazawa <t.kanazawa0810@gmail.com>
2023-08-18 19:18:38 +0900

    test

--- api/exportToSheet.js
+++ api/exportToSheet.js
@@ -1,49 +1,52 @@
 const { google } = require('googleapis');
 
-exports.handler = async (event) => {
-    if (event.httpMethod !== 'POST') {
-        return {
-            statusCode: 405,
-            body: 'Method Not Allowed'
-        };
-    }
+// 環境変数からGoogleの認証情報を取得
+const googleCredentials = {
+    type: process.env.GOOGLE_TYPE,
+    project_id: process.env.GOOGLE_PROJECT_ID,
+    private_key_id: process.env.GOOGLE_PRIVATE_KEY_ID,
+    private_key: process.env.GOOGLE_PRIVATE_KEY.replace(/\\n/g, '\n'),  // 正しい改行に変換
+    client_email: process.env.GOOGLE_CLIENT_EMAIL,
+    client_id: process.env.GOOGLE_CLIENT_ID,
+    auth_uri: process.env.GOOGLE_AUTH_URI,
+    token_uri: process.env.GOOGLE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.GOOGLE_AUTH_PROVIDER_X509_CERT_URL,
+    client_x509_cert_url: process.env.GOOGLE_CLIENT_X509_CERT_URL
+};
 
-    const body = JSON.parse(event.body);
-    const sheets = google.sheets('v4');
+const sheets = google.sheets({ version: 'v4' });
 
-    const credentials = require('./chat-bot-b7fb1-3b9bdbeb3468.json');
-
-    const auth = new google.auth.JWT(
-        credentials.client_email,
+/**
+ * Google Sheetsにデータをエクスポートする関数
+ * @param {Array} data - エクスポートするデータ。2次元配列を期待。
+ * @param {string} spreadsheetId - ターゲットとなるGoogle SheetsのID
+ * @param {string} range - エクスポート先の範囲 (例: 'Sheet1!A1')
+ */
+async function exportToSheet(data, spreadsheetId, range) {
+    // JWTクライアントの作成
+    const authClient = new google.auth.JWT(
+        googleCredentials.client_email,
         null,
-        credentials.private_key,
+        googleCredentials.private_key,
         ['https://www.googleapis.com/auth/spreadsheets']
     );
 
-    const spreadsheetId = '1PNZt48JtYPQKd56nxrW1ACG_Xhni9yE2nQwN93kgxlM';
-    const range = 'Sheet1!A1';
+    // トークンを取得
+    await authClient.authorize();
 
-    const rows = body.chatMessages.map(message => [message.sender, message.content]);
+    // Google Sheets APIを使用してデータを追加
+    const response = await sheets.spreadsheets.values.append({
+        auth: authClient,
+        spreadsheetId: spreadsheetId,
+        range: range,
+        valueInputOption: 'RAW',
+        insertDataOption: 'INSERT_ROWS',
+        resource: {
+            values: data
+        }
+    });
 
-    try {
-        await sheets.spreadsheets.values.append({
-            auth,
-            spreadsheetId,
-            range,
-            valueInputOption: 'RAW',
-            insertDataOption: 'INSERT_ROWS',
-            resource: {
-                values: rows
-            }
-        });
-        return {
-            statusCode: 200,
-            body: 'Exported Successfully'
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: 'Error exporting to Google Sheets'
-        };
-    }
-};
+    return response.data;
+}
+
+module.exports = exportToSheet;
