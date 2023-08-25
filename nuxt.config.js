3aabe967394793a13094fdca05df19ecb09f191b
Tsuyoshi Kanazawa <t.kanazawa0810@gmail.com>
2023-08-25 17:15:33 +0900

    test

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 export default {
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Scalar',
+    title: 'Scalar Ai assistant',
     htmlAttrs: {
       lang: 'jp'
     },
